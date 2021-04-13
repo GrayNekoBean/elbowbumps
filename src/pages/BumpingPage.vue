f90d584e9b121ee7823ccf46c13fcda0684f22bf
Zoya-Anwar <zoyaanwar@hotmail.com>
2021-04-13 01:18:29 +0100

    An attempt to put pending, accepted and matches on one page in tabs but the flickity gets annoyed

---
+++ src/pages/BumpingPage.vue
@@ -0,0 +1,47 @@
+<template>
+<section>
+
+<div class="profile-page">
+        <TabView class="profile-tab" style="margin-top: 3rem;">
+            <TabPanel header="New Matches" style="margin-top: 0rem;">
+                    <Matches> </Matches>
+            </TabPanel>
+            <TabPanel header="Pending Bumps">
+                <PendingBumps></PendingBumps>
+            </TabPanel>
+            <TabPanel header="Accepted Bumps">
+                <Bumps></Bumps>
+            </TabPanel>
+
+        </TabView>
+    
+</div>
+</section>
+</template>
+
+<script>
+
+import axios from "axios";
+import MarkdownIt from "markdown-it";
+
+
+import Matches from "./Matches";
+import PendingBumps from "./PendingBumps";
+import Bumps from "./Bumps";
+
+export default {
+    components: { Matches, PendingBumps, Bumps },
+   
+       
+}
+</script>
+
+<style lang="scss" scoped>
+
+$primitive-color: #a9edfe;
+$secondary-color: #ffaaaa;
+$background-color: #fffaba;
+
+
+
+</style>
